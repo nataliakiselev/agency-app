bed7efac72d4982c708803b530140086dbc275dc
James Sherry <james.m.sherry@googlemail.com>
2020-06-22 04:03:31 +0100

    you should use CSS to uppercase here...

--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -7,6 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   button: {
     color: "inherit",
+    textTransform: "uppercase",
     margin: theme.spacing(1),
   },
   root: {
@@ -23,39 +24,38 @@ const useStyles = makeStyles((theme) => ({
 }));
 const NavLinks = (props) => {
   const auth = useContext(AuthContext);
+  console.log("Nav logged in? ", auth.isLoggedIn);
   const classes = useStyles();
   return (
     <List className={classes.root}>
       <ListItem button>
         <NavLink to="/" exact>
-          TALENT
+          Talent {/* text should be lowercase and uppercased with CSS */}
         </NavLink>
       </ListItem>
       <ListItem button>
         <NavLink to="/users" exact>
-          AGENTS
+          Agents
         </NavLink>
       </ListItem>
 
       {auth.isLoggedIn && (
-        <ListItem button>
-          <NavLink to="/u1/profiles">MY PROFILES</NavLink>
-        </ListItem>
+        <>
+          <ListItem button>
+            <NavLink to="/u1/profiles">My Profiles</NavLink>
+          </ListItem>
+          <ListItem button>
+            <NavLink to="/profiles/new">Add Profile</NavLink>
+          </ListItem>
+          <ListItem button onClick={auth.logout}>
+            Logout
+          </ListItem>
+        </>
       )}
 
-      {auth.isLoggedIn && (
-        <ListItem button>
-          <NavLink to="/profiles/new">ADD PROFILE</NavLink>
-        </ListItem>
-      )}
       {!auth.isLoggedIn && (
         <ListItem button>
-          <NavLink to="/auth">LOG IN</NavLink>
-        </ListItem>
-      )}
-      {auth.isLoggedIn && (
-        <ListItem button onClick={auth.logout}>
-          LOGOUT
+          <NavLink to="/auth">LogIn</NavLink>
         </ListItem>
       )}
     </List>
