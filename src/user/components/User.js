5c58917ed74b5bf71cb06cfe23ed5f59e2e21f7b
James Sherry <james.m.sherry@googlemail.com>
2020-07-24 20:10:34 +0100

    corrected link url

--- src/user/components/User.js
+++ src/user/components/User.js
@@ -7,7 +7,7 @@ const User = (props) => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
-        <Link to={`/${props.id}/profiles`}>
+        <Link to={`/users/${props.id}/profiles`}>
           {/* <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
           </div> */}
